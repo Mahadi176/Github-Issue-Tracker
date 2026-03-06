29a254f2a9f6b5a37e05a974f59272d11bd86275
Mahadi176 <mahadi210176@gmail.com>
2026-03-07 03:27:12 +0600

    sign in page functional

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('geist')
+// GitHub signIn 
+const signInBtn = () => {
+    const userName = document.getElementById('username')
+    const passWord = document.getElementById('password')
+
+    const user = userName.value ;
+    const pass = passWord.value ;
+
+    if(user == 'admin' && pass == 'admin123'){
+        alert("Login Successful")
+        window.location.assign("/home.html")
+    }
+    else{
+        alert("Login Failed")
+        return ;
+    }
+}
+
+//Homepage 
